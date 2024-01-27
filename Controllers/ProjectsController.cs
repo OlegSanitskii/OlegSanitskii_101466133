4ce8df3435bd236aa0b0f7748f525187350e668e
Олег -Sanitskii <olegsanitskii@gmail.com>
2024-01-27 15:00:17 -0500

    test

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -10,10 +10,19 @@ namespace WebApplication2.Controllers
 {
     public class ProjectsController : Controller
     {
-        /*private static List<Project> _projects = new List<Project>()
+        /*
+         
+        
+        cv
+        
+        private static List<Project> _projects = new List<Project>()
         {
             new Project { ProjectId = 1, Name = "Project 1", Description = "First Project" }
-        };*/
+        };
+        
+         
+         
+         */
 
         public IActionResult Index()
         {
