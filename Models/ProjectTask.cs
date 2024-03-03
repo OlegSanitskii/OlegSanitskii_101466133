8796af9bcfe3df13a5a1af9ac3db070d803b9006
Олег -Sanitskii <olegsanitskii@gmail.com>
2024-03-03 16:17:09 -0500

    Some changes

--- Models/ProjectTask.cs
+++ Models/ProjectTask.cs
@@ -2,7 +2,7 @@
 {
     public class ProjectTask
     {
-        public int ProjectTaskId { get; set; }
+        public int ProjectTaskId { get; set; }  
         public string Title { get; set; }  
         public string Description { get; set; }
         public int ProjectId {  get; set; }
